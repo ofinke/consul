9e3147fe017a58f29288909cf523a7f9c197d469
Ondřej Finke <ondrej.finke@gmail.com>
2025-07-10 23:22:48 +0200

    drunk commit

--- src/consul/core/config/tasks.py
+++ src/consul/core/config/tasks.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from pathlib import Path
 
 import yaml
 from pydantic import BaseModel
@@ -26,5 +27,30 @@ class BaseTaskConfig(BaseModel):
     llm_params: LLMParameters = LLMParameters()
 
 
+CONFIG_MAPPING = {}
+
+
 def get_task_config(task: AvailableTasks) -> BaseTaskConfig:
+
+    try:
+        path = Path(__file__).parent / "../../../../configs/tasks/ask_question.yamls"
+        path = path.resolve()
+    except Exception as err:
+        print()
+
+    used_model = CONFIG_MAPPING.get(task, BaseTaskConfig)
+    used_model.model_validate()
+
+    match task:
+        # default case
+        case _:
+            return
+
     raise NotImplementedError
+
+
+get_task_config(AvailableTasks.ASK)
+
+# with Path.open(yaml_path, "r", encoding="utf-8") as file:
+#     config = yaml.safe_load(file)
+#     print()
